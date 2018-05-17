da2d26a1dc232ff887a28f7c298be1350b4f35e7
Rostislav Spinar <rostislav.spinar@microrisc.com>
2018-05-17 15:00:48 +0200

    CI update

--- .gitlab_build_files/copy_shape_libs.sh
+++ .gitlab_build_files/copy_shape_libs.sh
@@ -11,8 +11,8 @@ then
     exit 1
 fi
 
-echo "Shape folder ${LIB}${IQRFGD2} created."
 mkdir -p ${LIB}${IQRFGD2}
+echo "Shape folder ${LIB}${IQRFGD2} created."
 
 cp shape-build/bin/libTraceFormatService.so ${LIB}${IQRFGD2}
 cp shape-build/bin/libTraceFileService.so ${LIB}${IQRFGD2}
