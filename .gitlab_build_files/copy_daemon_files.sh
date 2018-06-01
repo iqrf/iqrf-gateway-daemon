06982d951c14497c25767d0fa226ebc35080a771
Rostislav Spinar <rostislav.spinar@iqrf.com>
2018-06-01 11:48:52 +0200

    Fix in CI helper

--- .gitlab_build_files/copy_daemon_files.sh
+++ .gitlab_build_files/copy_daemon_files.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-IQRFGD2_DEPLOY=$1
+DEPLOY=$1
 
 IQRFGD2_CFG=/etc/iqrfgd2
 IQRFGD2_BIN=/usr/bin
@@ -16,32 +16,32 @@ then
     exit 1
 fi
 
-mkdir -p ${LIB}${IQRFGD2_CFG}
-mkdir -p ${LIB}${IQRFGD2_BIN}
-mkdir -p ${LIB}${IQRFGD2_LIBS}
-mkdir -p ${LIB}${IQRFGD2_SHARE}
-mkdir -p ${LIB}${IQRFGD2_CACHE}
+mkdir -p ${DEPLOY}${IQRFGD2_CFG}
+mkdir -p ${DEPLOY}${IQRFGD2_BIN}
+mkdir -p ${DEPLOY}${IQRFGD2_LIBS}
+mkdir -p ${DEPLOY}${IQRFGD2_SHARE}
+mkdir -p ${DEPLOY}${IQRFGD2_CACHE}
 echo "Daemon folders created."
 
 # CFG
-cp iqrf-daemon-build/bin/configuration/*.json ${LIB}${IQRFGD2_CFG}
-cp -r iqrf-daemon-build/bin/configuration/jsonschema ${LIB}${IQRFGD2_CFG}
+cp iqrf-daemon-build/bin/configuration/*.json ${DEPLOY}${IQRFGD2_CFG}
+cp -r iqrf-daemon-build/bin/configuration/jsonschema ${DEPLOY}${IQRFGD2_CFG}
 
 # BIN
-cp iqrf-daemon-build/bin/start-IqrfDaemon ${LIB}${IQRFGD2_BIN}/iqrfgd2
-cp iqrf-daemon-build/bin/*.so ${LIB}${IQRFGD2_LIBS}
+cp iqrf-daemon-build/bin/start-IqrfDaemon ${DEPLOY}${IQRFGD2_BIN}/iqrfgd2
+cp iqrf-daemon-build/bin/*.so ${DEPLOY}${IQRFGD2_LIBS}
 
 # SHARE
-cp -r iqrf-daemon-build/bin/configuration/JsonSchemas ${LIB}${IQRFGD2_SHARE}
-cp -r iqrf-daemon-build/bin/configuration/JavaScript ${LIB}${IQRFGD2_SHARE}
+cp -r iqrf-daemon-build/bin/configuration/JsonSchemas ${DEPLOY}${IQRFGD2_SHARE}
+cp -r iqrf-daemon-build/bin/configuration/JavaScript ${DEPLOY}${IQRFGD2_SHARE}
 
 # CACHE
-cp -r iqrf-daemon-build/bin/configuration/Scheduler ${LIB}${IQRFGD2_CACHE}
+cp -r iqrf-daemon-build/bin/configuration/Scheduler ${DEPLOY}${IQRFGD2_CACHE}
 
 # SHAPE
-cp shape-libs/usr/lib/iqrfgd2/* ${LIB}${IQRFGD2_LIBS}
+cp shape-libs/usr/lib/iqrfgd2/* ${DEPLOY}${IQRFGD2_LIBS}
 
 # SHAPEWARE
-cp shapeware-libs/usr/lib/iqrfgd2/* ${LIB}${IQRFGD2_LIBS}
+cp shapeware-libs/usr/lib/iqrfgd2/* ${DEPLOY}${IQRFGD2_LIBS}
 
 echo "Daemon files copied."
