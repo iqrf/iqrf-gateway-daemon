f4475acd07b1ede7efd9b034c25e52eabdd2a7a3
Rostislav Spinar <rostislav.spinar@iqrf.com>
2018-06-08 10:59:41 +0200

    CI - fix in script for deb package, use ARCH variable in the name

--- .gitlab_build_files/create_daemon_package.sh
+++ .gitlab_build_files/create_daemon_package.sh
@@ -6,11 +6,11 @@ fpm \
 -s dir \
 -C iqrf-daemon-deploy \
 --name "iqrf-gateway-daemon" \
---version $IQRF_GW_DAEMON_VERSION \
+--version ${IQRF_GW_DAEMON_VERSION} \
 --license "Apache License, Version 2.0" \
 --vendor "IQRF Tech s.r.o." \
 --category "devel" \
---architecture "amd64" \
+--architecture ${ARCH} \
 --maintainer "Rostislav Spinar <rostislav.spinar@iqrf.com>" \
 --url "https://github.com/iqrfsdk/iqrf-gateway-daemon" \
 --description "IQRF Gateway Daemon v2 with the multiple communication channels - UDP/MQ/MQTT/WS." \
