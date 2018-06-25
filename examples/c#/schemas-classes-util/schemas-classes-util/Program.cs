38785bf7c129721a4aaea524e3a57472a15f4776
Rostislav Spinar <rostislav.spinar@iqrf.com>
2018-06-25 18:45:30 +0200

    Slightly improved C# util

--- examples/c#/schemas-classes-util/schemas-classes-util/Program.cs
+++ examples/c#/schemas-classes-util/schemas-classes-util/Program.cs
@@ -13,25 +13,53 @@ namespace schemas_classes_util
   {
     static void Main(string[] args)
     {
-      // json schemas
+      // load json schemas
       DirectoryInfo d = new DirectoryInfo(@"..\\..\\..\\..\\..\\..\\api");
       FileInfo[] Files = d.GetFiles("*.json*");
 
+      // fix directory for writing classes
+      Directory.SetCurrentDirectory(@"..\\..\\..");
+      Console.WriteLine("Current directory: {0}", Directory.GetCurrentDirectory());
+
+      // loop via schemas
       foreach (FileInfo file in Files)
       {
-        if (file.Name.Contains("Coordinator"))
+        if (file.Name.Contains("iqmeshNetwork"))
         {
+          // read json schema
           var schemaJson = File.ReadAllText(file.FullName);
 
+          // generate class
           var schema = JsonSchema4.FromJsonAsync(schemaJson).Result;
           var generator = new CSharpGenerator(schema);
           var generatedFile = generator.GenerateFile();
 
-          string s = file.FullName;
+          // class name
+          string s = file.Name;
           s = s.Replace("json", "cs");
 
-          // classes\embedCoordinator\*.cs
-          File.WriteAllText(s, generatedFile);
+          // ...classes\iqmeshNetwork\*.cs
+          string destPath = Path.Combine(Directory.GetCurrentDirectory() + "\\classes\\iqmeshNetwork", s);
+          File.WriteAllText(destPath, generatedFile);
+        }
+
+        if (file.Name.Contains("iqrfEmbedCoordinator"))
+        {
+          // read json schema
+          var schemaJson = File.ReadAllText(file.FullName);
+
+          // generate class
+          var schema = JsonSchema4.FromJsonAsync(schemaJson).Result;
+          var generator = new CSharpGenerator(schema);
+          var generatedFile = generator.GenerateFile();
+
+          // class name
+          string s = file.Name;
+          s = s.Replace("json", "cs");
+
+          // ...classes\iqrfEmbedCoordinator\*.cs
+          string destPath = Path.Combine(Directory.GetCurrentDirectory() + "\\classes\\iqrfEmbedCoordinator", s);
+          File.WriteAllText(destPath, generatedFile);
         }
         //...
       }
