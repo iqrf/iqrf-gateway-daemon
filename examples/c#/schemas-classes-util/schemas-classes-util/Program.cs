6879d469772f109cd508d1fb85b273e45136d4c8
Rostislav Spinar <rostislav.spinar@iqrf.com>
2018-06-25 15:28:45 +0200

    Util to generate C# classes from JSON schemas

---
+++ examples/c#/schemas-classes-util/schemas-classes-util/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace schemas_classes_util
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // json schemas
+      DirectoryInfo d = new DirectoryInfo(@"..\..\api");
+      FileInfo[] Files = d.GetFiles("*.json*");
+
+      foreach (FileInfo file in Files)
+      {
+        if (file.Name.Contains("Coordinator"))
+        {
+          var schemaJson = File.ReadAllText(file.FullName);
+
+          var schema = JsonSchema4.FromJsonAsync(schemaJson).Result;
+          var generator = new CSharpGenerator(schema);
+          var generatedFile = generator.GenerateFile();
+
+          string s = file.FullName;
+          s = s.Replace("json", "cs");
+
+          // classes\embedCoordinator\*.cs
+          File.WriteAllText(s, generatedFile);
+        }
+        //...
+      }
+    }
+  }
+}
