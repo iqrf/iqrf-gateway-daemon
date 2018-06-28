07e85e79f08cdba3eecde586b5118ae1c058bfd8
Rostislav Spinar <rostislav.spinar@iqrf.com>
2018-06-28 20:58:37 +0200

    Class generator is in utils

--- examples/c#/test-api-app/test-api-app/Program.cs
+++ examples/c#/test-api-app/test-api-app/Program.cs
@@ -126,71 +126,12 @@
     }
 
     /// <summary>
-    /// Generate cs class from JSON schema
-    /// </summary>
-    /// <param name="dirAPI"></param>
-    /// <param name="dirClass"></param>
-    private static void generateCSClassFromJSON ( string dirAPI, string dirClass )
-    {
-      Console.WriteLine( "Generating files.." );
-      // Generate C# class from the json schema      
-      try
-      {
-        DirectoryInfo d = new DirectoryInfo( dirAPI );
-        FileInfo[] Files = d.GetFiles( "*.json*" ); //Getting Text files
-        foreach ( FileInfo file in Files )
-        {
-          if ( !file.Name.Contains( "mngSched" ) )
-           {
-            var schemaJson = File.ReadAllText( file.FullName );
-            var schema = JsonSchema4.FromJsonAsync( schemaJson ).Result;
-            string fName = file.FullName;
-            string nameSpace = file.Name.Substring( 0, file.Name.IndexOf( '-', 0 ) );
-            string dir = nameSpace;
-            if ( dir.Contains( '_' ) )
-              dir = dir.Substring( 0, dir.IndexOf( '_', 0 ) );
-            if ( file.Name.Contains( "request" ) )
-            {
-              nameSpace += "_Request";
-            }
-            if ( file.Name.Contains( "response" ) )
-            {
-              nameSpace += "_Response";
-            }
-            string className = char.ToUpper( nameSpace[0] ) + nameSpace.Substring( 1 );
-            var generator = new CSharpGenerator( schema, new CSharpGeneratorSettings
-            {
-              ClassStyle = CSharpClassStyle.Poco,
-              Namespace = nameSpace,
-              SchemaType = SchemaType.Swagger2,
-            } );
-            if ( !Directory.Exists( dirClass + dir ) )
-              Directory.CreateDirectory( dirClass + dir );
-            Console.Write( "Generating " + file.Name.Replace( "json", "cs" ) + ".." );
-            var generatedFile = generator.GenerateFile( className );
-            string targetFile = dirClass + dir + '\\' + file.Name.Replace( "json", "cs" );
-            File.WriteAllText( targetFile, generatedFile );
-            Console.WriteLine( " OK" );
-          }
-        }
-      }
-      catch(Exception ex)
-      {
-        Console.WriteLine( ex.Message );
-      }
-      Console.Write( "Completed.." );
-    }
-
-    /// <summary>
     /// Main function
     /// </summary>
     static void Main ()
     {    
       try
       {
-        // Generate c# classes from JSON schema
-        //generateCSClassFromJSON( @"c:\Microrisc\Bastl\iqrfgd2_test_app\iqrfgd2_API_test\api\", @"c:\Microrisc\Bastl\iqrfgd2_test_app\iqrfgd2_API_test\" );
-                
         // Create request 
         IqrfRaw_Request request = new IqrfRaw_Request();
         request.Data.MsgId = "test";
