91c3f7fbf922fe634b464af704b6bd79d9456a9d
Rostislav Spinar <rostislav.spinar@iqrf.com>
2018-07-26 15:56:43 +0200

    Update in CI - use pipeline ID in DEB version

--- .gitlab_build_files/create_daemon_package.sh
+++ .gitlab_build_files/create_daemon_package.sh
@@ -6,7 +6,7 @@ fpm \
 -s dir \
 -C iqrf-daemon-deploy \
 --name "iqrf-gateway-daemon" \
---version ${IQRF_GW_DAEMON_VERSION} \
+--version ${IQRF_GW_DAEMON_VERSION}${CI_PIPELINE_ID} \
 --license "Apache License, Version 2.0" \
 --vendor "IQRF Tech s.r.o." \
 --category "devel" \
