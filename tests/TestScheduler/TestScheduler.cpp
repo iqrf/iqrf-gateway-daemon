bc2f22e0d93af658987fcc13a217778e5130f75f
Frantisek Mikulu <frantisek.mikulu@logimic.com>
2018-08-28 11:31:31 +0200

    TestScheduler basic tests

--- tests/TestScheduler/TestScheduler.cpp
+++ tests/TestScheduler/TestScheduler.cpp
@@ -1,7 +1,15 @@
 #include "TestScheduler.h"
 #include "Trace.h"
 #include "GTestStaticRunner.h"
+#include "HexStringCoversion.h"
+
 #include "gtest/gtest.h"
+
+#include "rapidjson/pointer.h"
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/prettywriter.h"
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -78,12 +86,6 @@ namespace iqrf {
       }
     }
 
-    void taskHandler(const rapidjson::Value& task)
-    {
-      TRC_FUNCTION_ENTER("");
-      TRC_FUNCTION_LEAVE("")
-    }
-
   };
 
   ////////////////////////////////////
@@ -144,6 +146,12 @@ namespace iqrf {
   {
   protected:
     const std::string CLIENT_ID = "TestScheduler";
+    const int VAL1 = 1;
+    const int VAL2 = 2;
+    const std::string CRON1 = "2 * * * * * *";
+    const std::string CRON2 = "20 * * * * * *";
+    const int PERIOD1 = 10;
+
     iqrf::ISchedulerService* m_iSchedulerService = nullptr;
 
     void SetUp(void) override
@@ -166,14 +174,176 @@ namespace iqrf {
       TRC_FUNCTION_LEAVE("")
     }
 
+    //for debug only
+    static std::string JsonToStr(const rapidjson::Value* val)
+    {
+      rapidjson::Document doc;
+      doc.CopyFrom(*val, doc.GetAllocator());
+      rapidjson::StringBuffer buffer;
+      rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+      doc.Accept(writer);
+      return buffer.GetString();
+    }
+
+    void checkTimeSpec(
+      const rapidjson::Value *timeSpec,
+      const std::string& cronTimeStr,
+      bool exactTimeB,
+      bool periodicB,
+      int periodI,
+      const std::string& startTimeStr
+      )
+    {
+      using namespace rapidjson;
+      using namespace chrono;
+
+      const rapidjson::Value *cronTime = Pointer("/cronTime").Get(*timeSpec);
+      const rapidjson::Value *exactTime = Pointer("/exactTime").Get(*timeSpec);
+      const rapidjson::Value *periodic = Pointer("/periodic").Get(*timeSpec);
+      const rapidjson::Value *period = Pointer("/period").Get(*timeSpec);
+      const rapidjson::Value *startTime = Pointer("/startTime").Get(*timeSpec);
+
+      ASSERT_NE(nullptr, cronTime);
+      ASSERT_NE(nullptr, exactTime);
+      ASSERT_NE(nullptr, periodic);
+      ASSERT_NE(nullptr, period);
+      ASSERT_NE(nullptr, startTime);
+
+      ASSERT_TRUE(cronTime->IsString());
+      ASSERT_TRUE(exactTime->IsBool());
+      ASSERT_TRUE(periodic->IsBool());
+      ASSERT_TRUE(period->IsInt());
+      ASSERT_TRUE(startTime->IsString());
+
+      EXPECT_EQ(cronTimeStr, cronTime->GetString());
+      EXPECT_EQ(exactTimeB, exactTime->GetBool());
+      EXPECT_EQ(periodicB, periodic->GetBool());
+      EXPECT_EQ(periodI, period->GetInt());
+
+      //std::cout << ">>>>>>>>> " << SchedulerTesting::JsonToStr(startTime) << std::endl;
+      if (!startTimeStr.empty()) {
+        EXPECT_EQ(startTimeStr, startTime->GetString());
+      }
+    }
+
   };
 
-  TEST_F(SchedulerTesting, TestEmpty)
+  TEST_F(SchedulerTesting, empty)
   {
     std::vector<ISchedulerService::TaskHandle> taskHandleVect =  m_iSchedulerService->getMyTasks(CLIENT_ID);
     EXPECT_EQ(0, taskHandleVect.size());
+
     const rapidjson::Value *val = m_iSchedulerService->getMyTask(CLIENT_ID, 0);
     EXPECT_EQ(nullptr, val);
+
+    val = m_iSchedulerService->getMyTaskTimeSpec(CLIENT_ID, 0);
+    EXPECT_EQ(nullptr, val);
   }
+
+  TEST_F(SchedulerTesting, scheduleTask)
+  {
+    using namespace rapidjson;
+    Document doc1;
+    Pointer("/item").Set(doc1, VAL1);
+    Document doc2;
+    Pointer("/item").Set(doc2, VAL2);
+    ISchedulerService::TaskHandle th1 = m_iSchedulerService->scheduleTask(CLIENT_ID, doc1, CRON1);
+    ISchedulerService::TaskHandle th2 = m_iSchedulerService->scheduleTask(CLIENT_ID, doc2, CRON2);
+
+    std::vector<ISchedulerService::TaskHandle> taskHandleVect = m_iSchedulerService->getMyTasks(CLIENT_ID);
+    ASSERT_EQ(2, taskHandleVect.size());
+    //tasks ordered according scheduled time
+    EXPECT_TRUE(th1 == taskHandleVect[0] || th1 == taskHandleVect[1]);
+    EXPECT_TRUE(th2 == taskHandleVect[0] || th2 == taskHandleVect[1]);
+
+    const rapidjson::Value *task1 = m_iSchedulerService->getMyTask(CLIENT_ID, th1);
+    const rapidjson::Value *val1 = Pointer("/item").Get(*task1);
+    ASSERT_NE(nullptr, val1);
+    ASSERT_TRUE(val1->IsInt());
+    EXPECT_EQ(VAL1, val1->GetInt());
+
+    const rapidjson::Value *task2 = m_iSchedulerService->getMyTask(CLIENT_ID, th2);
+    const rapidjson::Value *val2 = Pointer("/item").Get(*task2);
+    ASSERT_NE(nullptr, val2);
+    ASSERT_TRUE(val2->IsInt());
+    EXPECT_EQ(VAL2, val2->GetInt());
+
+    const rapidjson::Value *timeSpec = m_iSchedulerService->getMyTaskTimeSpec(CLIENT_ID, th1);
+    SchedulerTesting::checkTimeSpec(timeSpec, CRON1, false, false, 0, "");
+
+    std::vector<ISchedulerService::TaskHandle> taskHandleVectRem = { th1, th2 };
+    m_iSchedulerService->removeTasks(CLIENT_ID, taskHandleVectRem);
+    taskHandleVect = m_iSchedulerService->getMyTasks(CLIENT_ID);
+    EXPECT_EQ(0, taskHandleVect.size());
+  }
+
+  TEST_F(SchedulerTesting, scheduleTaskAt)
+  {
+    using namespace rapidjson;
+    using namespace chrono;
+
+    Document doc1;
+    Pointer("/item").Set(doc1, VAL1);
+
+    system_clock::time_point tp = system_clock::now();
+    tp += milliseconds(10000); //expire in 10 sec
+    string tpStr = encodeTimestamp(tp);
+
+    ISchedulerService::TaskHandle th1 = m_iSchedulerService->scheduleTaskAt(CLIENT_ID, doc1, tp);
+    const rapidjson::Value *task1 = m_iSchedulerService->getMyTask(CLIENT_ID, th1);
+
+    const rapidjson::Value *val1 = Pointer("/item").Get(*task1);
+    ASSERT_NE(nullptr, val1);
+    ASSERT_TRUE(val1->IsInt());
+    EXPECT_EQ(VAL1, val1->GetInt());
+
+    const rapidjson::Value *timeSpec = m_iSchedulerService->getMyTaskTimeSpec(CLIENT_ID, th1);
+    SchedulerTesting::checkTimeSpec(timeSpec, "", true, false, 0, tpStr);
+
+    m_iSchedulerService->removeAllMyTasks(CLIENT_ID);
+    std::vector<ISchedulerService::TaskHandle> taskHandleVect = m_iSchedulerService->getMyTasks(CLIENT_ID);
+    EXPECT_EQ(0, taskHandleVect.size());
+  }
+
+  TEST_F(SchedulerTesting, scheduleTaskPeriodic)
+  {
+    using namespace rapidjson;
+    using namespace chrono;
+
+    Document doc1;
+    Pointer("/item").Set(doc1, VAL1);
+
+    system_clock::time_point tp = system_clock::now();
+    tp += milliseconds(10000); //start in 10 sec
+    string tpStr = encodeTimestamp(tp);
+
+    ISchedulerService::TaskHandle th1 = m_iSchedulerService->scheduleTaskPeriodic(CLIENT_ID, doc1, seconds(PERIOD1), tp);
+    const rapidjson::Value *task1 = m_iSchedulerService->getMyTask(CLIENT_ID, th1);
+
+    const rapidjson::Value *val1 = Pointer("/item").Get(*task1);
+    ASSERT_NE(nullptr, val1);
+    ASSERT_TRUE(val1->IsInt());
+    EXPECT_EQ(VAL1, val1->GetInt());
+
+    const rapidjson::Value *timeSpec = m_iSchedulerService->getMyTaskTimeSpec(CLIENT_ID, th1);
+    SchedulerTesting::checkTimeSpec(timeSpec, "", false, true, PERIOD1 * 1000, tpStr);
+
+    m_iSchedulerService->removeTask(CLIENT_ID, th1);
+    std::vector<ISchedulerService::TaskHandle> taskHandleVect = m_iSchedulerService->getMyTasks(CLIENT_ID);
+    EXPECT_EQ(0, taskHandleVect.size());
+  }
+
+  TEST_F(SchedulerTesting, scheduleTaskHandler)
+  {
+    using namespace rapidjson;
+    Document doc1;
+    Pointer("/item").Set(doc1, VAL1);
+    ISchedulerService::TaskHandle th1 = m_iSchedulerService->scheduleTask(CLIENT_ID, doc1, CRON1);
+
+    m_iSchedulerService->removeTask(CLIENT_ID, th1);
+    std::vector<ISchedulerService::TaskHandle> taskHandleVect = m_iSchedulerService->getMyTasks(CLIENT_ID);
+    EXPECT_EQ(0, taskHandleVect.size());
+  }
+
 
 }
