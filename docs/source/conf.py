db38aa44d593a30e9201438c490b3584f0bd7eb1
Rostislav Spinar <rostislav.spinar@iqrf.com>
2019-01-17 15:27:35 +0100

    Increase docs version to 2.1.0-beta

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'IQRF Tech s.r.o.'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = 'v2.0.0'
+release = 'v2.1.0-beta'
 
 
 # -- General configuration ---------------------------------------------------
