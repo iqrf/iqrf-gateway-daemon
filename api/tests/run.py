73a23a3e13b53dcc9084306a7a165ecc28aa09ed
Rostislav Spinar <rostislav.spinar@iqrf.com>
2019-09-27 14:27:56 +0200

    API auto tests - IQRF sensor, bo

--- api/tests/run.py
+++ api/tests/run.py
@@ -2,10 +2,12 @@
 
 from tavern.core import run
 
-failure = run('tests_coordinator.tavern.yaml', pytest_args=["-x"])
-failure |= run('tests_explore.tavern.yaml', pytest_args=["-x"])
-failure |= run('tests_leds.tavern.yaml', pytest_args=["-x"])
-failure |= run('tests_sensor.tavern.yaml', pytest_args=["-x"])
+failure = run('tests-node0_coordinator.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node0_leds.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node1_explore.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node1_sensor.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node2_bo.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node2_sensor.tavern.yaml', pytest_args=["-x"])
 
 if failure:
     print("Error running tests.")
