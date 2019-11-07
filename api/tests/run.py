fcec7bd7b2af2128c0ae71ff6e6b734fc27d65e6
Rostislav Spinar <rostislav.spinar@iqrf.com>
2019-11-07 16:30:39 +0100

    Adding more API tests

--- api/tests/run.py
+++ api/tests/run.py
@@ -2,11 +2,16 @@
 
 from tavern.core import run
 
-failure = run('tests-node0_coordinator.tavern.yaml', pytest_args=["-x"])
+failure = run('tests-broadcast_raw.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node0_coordinator.tavern.yaml', pytest_args=["-x"])
 failure |= run('tests-node0_leds.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node0_os.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node0_sfrc.tavern.yaml', pytest_args=["-x"])
 failure |= run('tests-node1_explore.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node1_iqmesh.tavern.yaml', pytest_args=["-x"])
 failure |= run('tests-node1_sensor.tavern.yaml', pytest_args=["-x"])
 failure |= run('tests-node2_bo.tavern.yaml', pytest_args=["-x"])
+failure |= run('tests-node2_iqmesh.tavern.yaml', pytest_args=["-x"])
 failure |= run('tests-node2_sensor.tavern.yaml', pytest_args=["-x"])
 failure |= run('tests-node3_light.tavern.yaml', pytest_args=["-x"])
 failure |= run('tests-node4_dali.tavern.yaml', pytest_args=["-x"])
