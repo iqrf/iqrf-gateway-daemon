5f4b3282a89338f2c7f1fe3ac801361635bc9d9e
Roman Ondráček <ondracek.roman@centrum.cz>
2020-08-05 12:36:17 +0200

    Debian packaging: refactor package build
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- buildPackage.sh
+++ buildPackage.sh
@@ -3,26 +3,6 @@
 
 set -e
 
-export DAEMON_VERSION=v2.1.0
-
-rm -rf shape shape-build shapeware shapeware-build
-mkdir shape-build shapeware-build
-
-# Clone shape repository
-git clone --recurse-submodules https://github.com/logimic/shape.git
-
-# Clone shapeware repository
-git clone --recurse-submodules https://github.com/logimic/shapeware.git
-
-# Build shape
-cmake -Bshape-build -Hshape -DCMAKE_SKIP_INSTALL_RPATH=TRUE -DCMAKE_SKIP_BUILD_RPATH=TRUE -DCMAKE_SKIP_RPATH=TRUE -DUSE_CCACHE=TRUE
-cmake --build shape-build
-
-# Build shapeware
-cmake -Bshapeware-build -Hshapeware -Dshape_DIR:PATH=`pwd`/shape-build -DLWS_STATIC_PIC:BOOL=TRUE \
--DLWS_WITH_SSL:BOOL=FALSE -DCMAKE_SKIP_INSTALL_RPATH=TRUE -DCMAKE_SKIP_BUILD_RPATH=TRUE -DCMAKE_SKIP_RPATH=TRUE -DUSE_CCACHE=TRUE
-cmake --build shapeware-build
-
 # Generate Debian changelog
 if [ ! -v BUILD ] || [ -z "$BUILD" ] || [ "$BUILD" != "release" ]; then
     gbp dch -a -S
@@ -31,4 +11,4 @@ else
 fi
 
 # Build Debian package
-debuild -e DAEMON_VERSION -b -rfakeroot -us -uc -tc
+debuild -e CCACHE_COMPILERCHECK -e CCACHE_COMPRESS -e CCACHE_BASEDIR -e CCACHE_DIR -e DAEMON_VERSION -b -uc -us -tc
