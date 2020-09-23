7b6dc3f4a41f75faeb0a2ed19a7fc9dfe828c641
Rostislav Spinar <rostislav.spinar@iqrf.com>
2020-09-23 17:59:32 +0200

    Changes for OPI-02
    - separate bus signal into more signals

--- src/IqrfUart/IqrfUart.cpp
+++ src/IqrfUart/IqrfUart.cpp
@@ -348,9 +348,27 @@ namespace iqrf {
 
         m_cfg.baudRate = get_baud(m_baudRate);
 
+        // default RPI mapping in include
+        m_cfg.powerEnableGpioPin = POWER_ENABLE_GPIO;
+        m_cfg.busEnableGpioPin = BUS_ENABLE_GPIO;
+        m_cfg.pgmSwitchGpioPin = PGM_SWITCH_GPIO;
+        m_cfg.trModuleReset = TR_MODULE_RESET_DISABLE;
+
         m_cfg.powerEnableGpioPin = (uint8_t)Pointer("/powerEnableGpioPin").GetWithDefault(d, (int)m_cfg.powerEnableGpioPin).GetInt();
         m_cfg.busEnableGpioPin = (uint8_t)Pointer("/busEnableGpioPin").GetWithDefault(d, (int)m_cfg.busEnableGpioPin).GetInt();
-        m_cfg.pgmSwitchGpioPin = -1;
+
+        // bus signal separated into more
+        if (m_cfg.busEnableGpioPin == -1) {
+          m_cfg.uartEnableGpioPin = (int8_t)Pointer("/uartEnableGpioPin").GetWithDefault(d, (int)m_cfg.uartEnableGpioPin).GetInt();
+          m_cfg.spiEnableGpioPin = (int8_t)Pointer("/spiEnableGpioPin").GetWithDefault(d, (int)m_cfg.spiEnableGpioPin).GetInt();
+          m_cfg.i2cEnableGpioPin = (int8_t)Pointer("/i2cEnableGpioPin").GetWithDefault(d, (int)m_cfg.i2cEnableGpioPin).GetInt();
+        }
+
+        m_cfg.pgmSwitchGpioPin = (uint8_t)Pointer("/pgmSwitchGpioPin").GetWithDefault(d, (int)m_cfg.pgmSwitchGpioPin).GetInt();
+
+        Value* v = Pointer("/uartReset").Get(d);
+        if (v && v->IsBool())
+          m_cfg.trModuleReset = v->GetBool() ? TR_MODULE_RESET_ENABLE : TR_MODULE_RESET_DISABLE;  
 
         TRC_INFORMATION(PAR(m_interfaceName) << PAR(m_baudRate));
 
