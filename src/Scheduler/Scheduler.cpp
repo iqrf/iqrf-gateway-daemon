34484fb4ba64500f2f5a540cd1927bdf7f64acf3
Karel Hanák <karel.hanak@iqrf.org>
2021-02-22 19:21:27 +0100

    Debian package: remove redundant schemas from filesystem
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/Scheduler/Scheduler.cpp
+++ src/Scheduler/Scheduler.cpp
@@ -65,8 +65,14 @@ namespace iqrf {
 
     using namespace rapidjson;
 
-    m_cacheDir = m_iLaunchService->getCacheDir() + "/scheduler";
-    m_schemaFile = m_iLaunchService->getDataDir() + "/schedulerSchemas/schema_cache_record.json";
+    std::string cacheDir = m_iLaunchService->getCacheDir();
+    m_cacheDir = cacheDir.empty() ? "." : cacheDir;
+    m_cacheDir += "/scheduler";
+
+    std::string schemaDir = m_iLaunchService->getDataDir();
+    m_schemaFile = schemaDir.empty() ? "." : schemaDir;
+    m_schemaFile += "/schedulerSchemas/schema_cache_record.json";
+    
     TRC_INFORMATION("Using cache dir: " << PAR(m_cacheDir));
     TRC_INFORMATION("Using record schema file: " << PAR(m_schemaFile));
     
