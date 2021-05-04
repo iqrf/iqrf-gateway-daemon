ccff5f2c87534f905e03c92ecb147d2d5cecb899
Karel Hanák <karel.hanak@iqrf.org>
2021-05-04 09:23:34 +0200

    added error handling for missing metadata when appending to responses
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/JsonDpaApiIqrfStandard/JsonDpaApiIqrfStandard.cpp
+++ src/JsonDpaApiIqrfStandard/JsonDpaApiIqrfStandard.cpp
@@ -206,7 +206,13 @@ namespace iqrf {
       // db metadata
       if (m_iIqrfInfo) {
         if (m_iIqrfInfo->getMidMetaDataToMessages()) {
-          com->setMidMetaData(m_iIqrfInfo->getNodeMetaData(com->getNadr()));
+          Document metaDoc;
+          try {
+            metaDoc = m_iIqrfInfo->getNodeMetaData(com->getNadr());
+          } catch (const std::exception &e) {
+            TRC_WARNING(e.what());
+          }
+          com->setMidMetaData(metaDoc);
         }
       }
 
@@ -394,7 +400,13 @@ namespace iqrf {
         // db metadata
         if (m_iIqrfInfo) {
           if (m_iIqrfInfo->getMidMetaDataToMessages()) {
-            com->setMidMetaData(m_iIqrfInfo->getNodeMetaData(com->getNadr()));
+            Document metaDoc;
+            try {
+              metaDoc = m_iIqrfInfo->getNodeMetaData(com->getNadr());
+            } catch (const std::exception &e) {
+              TRC_WARNING(e.what());
+            }
+            com->setMidMetaData(metaDoc);
           }
         }
 
