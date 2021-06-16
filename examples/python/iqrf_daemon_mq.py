da7c5c5c6f7d4844131c95cc9fa0caf51b55886f
Karel Hanák <karel.hanak@iqrf.org>
2021-06-16 12:20:10 +0200

    examples: added missing headers, moved C, C# and Java examples to separate repositories
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- examples/python/iqrf_daemon_mq.py
+++ examples/python/iqrf_daemon_mq.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 
-# #############################################################################
-# Author: 2016 Josef Hajek <hajek@rehivetech.com>                             #
-#         RehiveTech spol. s r.o., www.rehivetech.com                         #
-#                                                                             #
-# Author: 2018 Rostislav Spinar <rostislav.spinar@iqrf.com>                   #
-#         IQRF Tech s.r.o.                                                    #
-# #############################################################################
+# Copyright 2015-2021 IQRF Tech s.r.o.
+# Copyright 2019-2021 MICRORISC s.r.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # #############################################################################
 #                                                                             #
