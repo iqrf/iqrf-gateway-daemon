9fa8d54d3992e6dadefd59a3cc34ca1b1ba40501
Karel Hanák <karel.hanak@iqrf.org>
2022-03-13 06:44:28 +0100

    IQMESH: ping, restart, maintenance refactor
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/IqmeshServices/MaintenanceService/ComIqmeshNetworkMaintenance.h
+++ src/IqmeshServices/MaintenanceService/ComIqmeshNetworkMaintenance.h
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2015-2021 IQRF Tech s.r.o.
+ * Copyright 2019-2021 MICRORISC s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include "ComBase.h"
@@ -5,115 +21,119 @@
 #include <list>
 #include "JsonUtils.h"
 
+using namespace rapidjson;
+
+/// iqrf namespace
 namespace iqrf {
+	/// Maintenance input paramaters
+	typedef struct {
+		uint16_t deviceAddr = COORDINATOR_ADDRESS;
+		uint8_t RFchannel = 0;
+		uint8_t RFfilter = 0;
+		IDpaTransaction2::FrcResponseTime measurementTime = IDpaTransaction2::FrcResponseTime::k40Ms;
+		int measurementTimeMS = 40;
+		int repeat = 1;
+	} TMaintenanceInputParams;
 
-  // Maintenance input paramaters
-  typedef struct
-  {
-    uint16_t deviceAddr = COORDINATOR_ADDRESS;
-    uint8_t RFchannel = 0;
-    uint8_t RFfilter = 0;
-    IDpaTransaction2::FrcResponseTime measurementTime = IDpaTransaction2::FrcResponseTime::k40Ms;
-    int measurementTimeMS = 40;
-    int repeat = 1;
-  }TMaintenanceInputParams;
+	class ComIqmeshNetworkMaintenance : public ComBase {
+	public:
+		/**
+		 * Base constructor
+		 */
+		ComIqmeshNetworkMaintenance() = delete;
 
-  class ComIqmeshNetworkMaintenance : public ComBase
-  {
-  public:
-    ComIqmeshNetworkMaintenance() = delete;
-    explicit ComIqmeshNetworkMaintenance(rapidjson::Document& doc)
-      :ComBase(doc)
-    {
-      parse(doc);
-    }
+		/**
+		 * Constructor
+		 * @param doc Request document
+		 */
+		explicit ComIqmeshNetworkMaintenance(Document &doc) : ComBase(doc) {
+			parse(doc);
+		}
 
+		/**
+		 * Destructor
+		 */
+		virtual ~ComIqmeshNetworkMaintenance() {}
 
-    virtual ~ComIqmeshNetworkMaintenance()
-    {
-    }
+		/**
+		 * Returns maintenance request parameters
+		 * @return const TMaintenanceInputParams
+		 */
+		const TMaintenanceInputParams getMaintenanceInputParams() const {
+			return m_MaintenanceParams;
+		}
+	protected:
+		/**
+		 * Populates response document
+		 * @param doc Response document
+		 * @param res Transaction result
+		 */
+		void createResponsePayload(Document &doc, const IDpaTransactionResult2 &res) override {
+			Pointer("/data/rsp/response").Set(doc, encodeBinary(res.getResponse().DpaPacket().Buffer, res.getResponse().GetLength()));
+		}
 
-    const TMaintenanceInputParams getMaintenanceInputParams() const
-    {
-      return m_MaintenanceParams;
-    }
+	private:
+		/**
+		 * Parses reqeust document and stores maintenance request parameters
+		 * @param doc Request document
+		 */
+		void parse(rapidjson::Document &doc) {
+			Value *jsonVal;
+			// Repeat
+			if ((jsonVal = Pointer("/data/repeat").Get(doc))) {
+				m_MaintenanceParams.repeat = jsonVal->GetInt();
+			}
+			// deviceAddr
+			if ((jsonVal = Pointer("/data/req/deviceAddr").Get(doc))) {
+				m_MaintenanceParams.deviceAddr = (uint16_t)jsonVal->GetUint();
+			}
+			// RFchannel
+			if ((jsonVal = Pointer("/data/req/RFchannel").Get(doc))) {
+				m_MaintenanceParams.RFchannel = (uint8_t)jsonVal->GetUint();
+				if (m_MaintenanceParams.RFchannel > 67) {
+					m_MaintenanceParams.RFchannel = 0;
+				}
+			}
+			// RFfilter
+			if ((jsonVal = Pointer("/data/req/RFfilter").Get(doc))) {
+				m_MaintenanceParams.RFfilter = (uint8_t)jsonVal->GetUint();
+				if (m_MaintenanceParams.RFfilter > 64 && m_MaintenanceParams.RFfilter != 255) {
+					m_MaintenanceParams.RFfilter = 0;
+				}
+			}
+			// measurementTime
+			if ((jsonVal = Pointer("/data/req/measurementTime").Get(doc))) {
+				m_MaintenanceParams.measurementTimeMS = jsonVal->GetUint();
+				switch (m_MaintenanceParams.measurementTimeMS) {
+					case 360:
+						m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k360Ms;
+						break;
+					case 680:
+						m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k680Ms;
+						break;
+					case 1320:
+						m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k1320Ms;
+						break;
+					case 2600:
+						m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k2600Ms;
+						break;
+					case 5160:
+						m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k5160Ms;
+						break;
+					case 10280:
+						m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k10280Ms;
+						break;
+					case 20620:
+						m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k20620Ms;
+						break;
+					default:
+						m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k40Ms;
+						break;
+				}
+			}
+		}
 
-  protected:
-    void createResponsePayload(rapidjson::Document& doc, const IDpaTransactionResult2& res) override
-    {
-      rapidjson::Pointer("/data/rsp/response")
-        .Set(doc, encodeBinary(res.getResponse().DpaPacket().Buffer, res.getResponse().GetLength()));
-    }
-
-
-  private:
-    TMaintenanceInputParams m_MaintenanceParams;
-
-    // parses document into data fields
-    void parse(rapidjson::Document& doc) 
-    {
-      rapidjson::Value* jsonVal;
-
-      // Repeat
-      if ((jsonVal = rapidjson::Pointer("/data/repeat").Get(doc)))
-        m_MaintenanceParams.repeat = jsonVal->GetInt();
-
-      // deviceAddr
-      if ((jsonVal = rapidjson::Pointer("/data/req/deviceAddr").Get(doc)))
-      {
-        m_MaintenanceParams.deviceAddr = (uint16_t)jsonVal->GetInt();
-        if (m_MaintenanceParams.deviceAddr != COORDINATOR_ADDRESS && m_MaintenanceParams.deviceAddr != BROADCAST_ADDRESS)
-          m_MaintenanceParams.deviceAddr = COORDINATOR_ADDRESS;
-      }
-
-      // RFchannel
-      if ((jsonVal = rapidjson::Pointer("/data/req/RFchannel").Get(doc)))
-      {
-        m_MaintenanceParams.RFchannel = (uint8_t)jsonVal->GetInt();
-        if (m_MaintenanceParams.RFchannel > 67)
-          m_MaintenanceParams.RFchannel = 0;
-      }
-
-      // RFfilter
-      if ((jsonVal = rapidjson::Pointer("/data/req/RFfilter").Get(doc)))
-      {
-        m_MaintenanceParams.RFfilter = (uint8_t)jsonVal->GetInt();
-        if (m_MaintenanceParams.RFfilter > 64 && m_MaintenanceParams.RFfilter != 255)
-          m_MaintenanceParams.RFfilter = 0;
-      }
-
-      // measurementTime
-      if ((jsonVal = rapidjson::Pointer("/data/req/measurementTime").Get(doc)))
-      {
-        m_MaintenanceParams.measurementTimeMS = jsonVal->GetInt();
-        switch (m_MaintenanceParams.measurementTimeMS)
-        {
-        case 360:
-          m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k360Ms;
-          break;
-        case 680:
-          m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k680Ms;
-          break;
-        case 1320:
-          m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k1320Ms;
-          break;
-        case 2600:
-          m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k2600Ms;
-          break;
-        case 5160:
-          m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k5160Ms;
-          break;
-        case 10280:
-          m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k10280Ms;
-          break;
-        case 20620:
-          m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k20620Ms;
-          break;
-        default:
-          m_MaintenanceParams.measurementTime = IDpaTransaction2::FrcResponseTime::k40Ms;
-          break;
-        }        
-      }
-    }
-  };
+		/// maintenance request parameters
+		TMaintenanceInputParams m_MaintenanceParams;
+	};
 }
--- src/IqmeshServices/RestartService/ComIqmeshNetworkRestart.h
+++ src/IqmeshServices/RestartService/ComIqmeshNetworkRestart.h
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2015-2021 IQRF Tech s.r.o.
+ * Copyright 2019-2021 MICRORISC s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include "ComBase.h"
@@ -5,58 +21,70 @@
 #include <list>
 #include "JsonUtils.h"
 
+using namespace rapidjson;
+
+/// iqrf namespace
 namespace iqrf {
+	/// Restart input paramaters
+	typedef struct {
+		uint16_t hwpId = HWPID_DoNotCheck;
+		int repeat = 1;
+	} TRestartInputParams;
 
-  // Restart input paramaters
-  typedef struct
-  {
-    uint16_t hwpId = HWPID_DoNotCheck;
-    int repeat = 1;
-  }TRestartInputParams;
+	class ComIqmeshNetworkRestart : public ComBase {
+	public:
+		/**
+		 * Base constructor
+		 */
+		ComIqmeshNetworkRestart() = delete;
 
-  class ComIqmeshNetworkRestart : public ComBase
-  {
-  public:
-    ComIqmeshNetworkRestart() = delete;
-    explicit ComIqmeshNetworkRestart(rapidjson::Document& doc)
-      :ComBase(doc)
-    {
-      parse(doc);
-    }
+		/**
+		 * Constructor
+		 * @param doc Request document
+		 */
+		explicit ComIqmeshNetworkRestart(Document &doc) : ComBase(doc) {
+			parse(doc);
+		}
 
+		/**
+		 * Destructor
+		 */
+		virtual ~ComIqmeshNetworkRestart() {}
 
-    virtual ~ComIqmeshNetworkRestart()
-    {
-    }
+		/**
+		 * Returns restart request parameters
+		 * @return Restart request parameters
+		 */
+		const TRestartInputParams getRestartInputParams() const {
+			return m_RestartParams;
+		}
+	protected:
+		/**
+		 * Populates response document
+		 * @param doc Response document
+		 * @param res Transaction result
+		 */
+		void createResponsePayload(Document &doc, const IDpaTransactionResult2 &res) override {
+			Pointer("/data/rsp/response").Set(doc, encodeBinary(res.getResponse().DpaPacket().Buffer, res.getResponse().GetLength()));
+		}
+	private:
+		/**
+		 * Parses request document and stores restart request parameters
+		 * @param doc Request document
+		 */
+		void parse(Document &doc) {
+			Value *jsonVal;
+			// Repeat
+			if ((jsonVal = Pointer("/data/repeat").Get(doc))) {
+				m_RestartParams.repeat = jsonVal->GetInt();
+			}
+			// HWPID
+			if ((jsonVal = Pointer("/data/req/hwpId").Get(doc))) {
+				m_RestartParams.hwpId = (uint16_t)jsonVal->GetUint();
+			}
+		}
 
-    const TRestartInputParams getRestartInputParams() const
-    {
-      return m_RestartParams;
-    }
-
-  protected:
-    void createResponsePayload(rapidjson::Document& doc, const IDpaTransactionResult2& res) override
-    {
-      rapidjson::Pointer("/data/rsp/response")
-        .Set(doc, encodeBinary(res.getResponse().DpaPacket().Buffer, res.getResponse().GetLength()));
-    }
-
-
-  private:
-    TRestartInputParams m_RestartParams;
-
-    // parses document into data fields
-    void parse(rapidjson::Document& doc) 
-    {
-      rapidjson::Value* jsonVal;
-
-      // Repeat
-      if ((jsonVal = rapidjson::Pointer("/data/repeat").Get(doc)))
-        m_RestartParams.repeat = jsonVal->GetInt();
-
-      // HWPID
-      if ((jsonVal = rapidjson::Pointer("/data/req/hwpId").Get(doc)))
-        m_RestartParams.hwpId = (uint16_t)jsonVal->GetInt();
-    }
-  };
+		/// restart request parameters
+		TRestartInputParams m_RestartParams;
+	};
 }
