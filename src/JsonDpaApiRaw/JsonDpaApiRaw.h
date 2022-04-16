379106256c3143abe7b13b7ea387181a00080bee
Karel Hanák <karel.hanak@iqrf.org>
2022-04-16 11:36:52 +0200

    added IqrfDb and JsonDbApi components, added sqlite_orm and nlohmann_json libraries
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/JsonDpaApiRaw/JsonDpaApiRaw.h
+++ src/JsonDpaApiRaw/JsonDpaApiRaw.h
@@ -16,39 +16,138 @@
  */
 #pragma once
 
-#include "IIqrfInfo.h"
+#include "ComRaws.h"
+#include "FakeAsyncTransactionResult.h"
+#include "IIqrfDb.h"
 #include "IIqrfDpaService.h"
 #include "IMessagingSplitterService.h"
 #include "ShapeProperties.h"
 #include "IMessagingService.h"
+#include "ITemplateService.h"
 #include "ITraceService.h"
+#include "ObjectFactory.h"
+#include "Trace.h"
+
+#include "rapidjson/rapidjson.h"
+#include "rapidjson/document.h"
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/prettywriter.h"
+
+#include <algorithm>
+#include <fstream>
 #include <map>
 
 namespace iqrf {
-  class JsonDpaApiRaw
-  {
-  public:
-    JsonDpaApiRaw();
-    virtual ~JsonDpaApiRaw();
 
-    void activate(const shape::Properties *props = 0);
-    void deactivate();
-    void modify(const shape::Properties *props);
+	class JsonDpaApiRaw {
+	public:
+		/**
+		 * Constructor
+		 */
+		JsonDpaApiRaw();
 
-    void attachInterface(IIqrfInfo* iface);
-    void detachInterface(IIqrfInfo* iface);
+		/**
+		 * Destructor
+		 */
+		virtual ~JsonDpaApiRaw();
 
-    void attachInterface(IIqrfDpaService* iface);
-    void detachInterface(IIqrfDpaService* iface);
+		/**
+		 * Activate component instance
+		 * @param props Instance properteis
+		 */
+		void activate(const shape::Properties *props = 0);
 
-    void attachInterface(IMessagingSplitterService* iface);
-    void detachInterface(IMessagingSplitterService* iface);
+		/**
+		 * Modify component instance members
+		 * @param props Instance properties
+		 */
+		void modify(const shape::Properties *props);
 
-    void attachInterface(shape::ITraceService* iface);
-    void detachInterface(shape::ITraceService* iface);
+		/**
+		 * Deactivate component instance
+		 */
+		void deactivate();
 
-  private:
-    class Imp;
-    Imp* m_imp;
-  };
+		/**
+		 * Attach DB service interface
+		 * @param iface DB service interface
+		 */
+		void attachInterface(IIqrfDb *iface);
+
+		/**
+		 * Detach DB service interface
+		 * @param iface DB service interface
+		 */
+		void detachInterface(IIqrfDb *iface);
+
+		/**
+		 * Attach DPA service interface
+		 * @param iface DPA service interface
+		 */
+		void attachInterface(IIqrfDpaService* iface);
+
+		/**
+		 * Detach DPA service interface
+		 * @param iface DPA service interface
+		 */
+		void detachInterface(IIqrfDpaService* iface);
+
+		/**
+		 * Attach Splitter service interface
+		 * @param iface Splitter service interface
+		 */
+		void attachInterface(IMessagingSplitterService* iface);
+
+		/**
+		 * Detach Splitter service interface
+		 * @param iface Splitter service interface
+		 */
+		void detachInterface(IMessagingSplitterService* iface);
+
+		/**
+		 * Attach Tracing service interface
+		 * @param iface Tracing service interface
+		 */
+		void attachInterface(shape::ITraceService* iface);
+
+		/**
+		 * Detach Tracing service interface
+		 * @param iface Tracing service interface
+		 */
+		void detachInterface(shape::ITraceService* iface);
+
+	private:
+		/**
+		 * Handles DPA messages
+		 * @param messagingId Messaging ID
+		 * @param msgType Message type
+		 * @param doc Request document
+		 */
+		void handleMsg(const std::string &messagingId, const IMessagingSplitterService::MsgType &msgType, rapidjson::Document doc);
+
+		/**
+		 * Handles asynchronous DPA messages
+		 * @param msg DPA message
+		 */
+		void handleAsyncDpaMessage(const DpaMessage &msg);
+
+		/// DB service interface
+		IIqrfDb *m_dbService = nullptr;
+		/// Splitter service interface
+		IMessagingSplitterService *m_splitterService = nullptr;
+		/// DPA service interface
+		IIqrfDpaService *m_dpaService = nullptr;
+		/// Default instance name
+		std::string m_instanceName = "JsonDpaApiRaw";
+		/// Indicates whether DPA message is asynchronous
+		bool m_asyncDpaMessage = false;
+		/// DPA message object factory
+		ObjectFactory<ComNadr, rapidjson::Document&> m_objectFactory;
+		/// Vector of handled DPA messages
+		std::vector<std::string> m_filters = {
+			"iqrfRaw",
+			"iqrfRawHdp",
+		};
+	};
 }
