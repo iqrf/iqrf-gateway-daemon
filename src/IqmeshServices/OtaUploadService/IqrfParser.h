497a41f7a470a32b25688dd7efe21c3f72bf2fa9
Karel Hanák <karel.hanak@iqrf.org>
2022-07-30 15:24:07 +0200

    OtaUploadService: updated validation and reporting of hex and iqrf files

--- src/IqmeshServices/OtaUploadService/IqrfParser.h
+++ src/IqmeshServices/OtaUploadService/IqrfParser.h
@@ -41,11 +41,45 @@ namespace iqrf {
 				throw std::logic_error("Unable to open file " + filename + ": " + std::strerror(errno));
 			}
 
+			uint32_t linenum = 0;
 			std::string record;
 			while (std::getline(file, record)) {
 				StringUtils::trim(record);
 				if (record.empty()) {
 					continue;
+				}
+				linenum++;
+				try {
+					if (linenum <= 5 && !StringUtils::starsWith(record, "#")) {
+						throw std::logic_error("IQRF plugins should start with 5 header records.");
+					}
+					if (StringUtils::starsWith(record, "#")) {
+						switch (linenum) {
+						case 1:
+							ihp::iqrf::parseMcuHeader(record, m_mcu, m_tr);
+							break;
+						case 2:
+							ihp::iqrf::parseOsHeader(record, m_os);
+							break;
+						case 3: //date header
+							break;
+						case 4:
+							if (ihp::iqrf::validPluginHeaderOs(record)) {
+								throw std::logic_error("Regular ChangeOS plugin cannot be uploaded via OTA upload service.");
+							}
+							break;
+						case 5: // separator
+						default:
+							if (!ihp::iqrf::isSeparator(record)) {
+								throw std::logic_error("IQRF plugins should have only 4 programming headers and separator.");
+							}
+							break;
+						}
+						continue;
+					}
+					ihp::iqrf::validateData(record);
+				} catch (const std::logic_error &e) {
+					throw std::logic_error(std::string(e.what()) + " (" + basename(filename.c_str()) + ":" + std::to_string(linenum) + ")");
 				}
 				m_contents.push_back(record);
 			}
@@ -87,35 +121,7 @@ namespace iqrf {
 		 */
 		CodeBlock parse() {
 			std::basic_string<uint8_t> data;
-			uint32_t cnt = 0;
-
 			for (auto &record : m_contents) {
-				cnt++;
-				if (StringUtils::starsWith(record, "#")) {
-					switch (cnt) {
-					case 1:
-						ihp::iqrf::parseMcuHeader(record, m_mcu, m_tr);
-						break;
-					case 2:
-						ihp::iqrf::parseOsHeader(record, m_os);
-						break;
-					case 3: //date header
-						break;
-					case 4:
-						if (ihp::iqrf::validPluginHeaderOs(record)) {
-							throw std::logic_error("Regular ChangeOS plugin cannot be uploaded via OTA upload service.");
-						}
-						break;
-					case 5: // separator
-					default:
-						if (!ihp::iqrf::isSeparator(record)) {
-							throw std::logic_error("IQRF plugins should have only 4 programming headers and separator.");
-						}
-						break;
-					}
-					continue;
-				}
-				ihp::iqrf::validateData(record);
 				for (uint32_t i = 0; i < ihp::iqrf::LINE_LENGTH; i += 2) {
 					uint8_t byte = ihp::utils::hexStringToByte(record, i);
 					data.push_back(byte);
