5dfd533fd6af38e8a2707c3de8a749a6e733b857
Marek Belisko <marek.belisko@open-nandra.com>
2023-06-09 12:47:08 +0200

    test:task_queue: Added some tests
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>

--- test/task_queue.cpp
+++ test/task_queue.cpp
@@ -1,9 +1,64 @@
 #include <string>
+#include  "TaskQueue.h"
 
 #include <gtest/gtest.h>
 
+// global for results
+std::string result, result_delayed;
 
-TEST(flags8, data_types) {
-    EXPECT_EQ(0x1, 0x15);
+void handleQueue(std::string a) {
+    result += a;
 }
 
+void handleQueueDelayed(std::string a) {
+    result_delayed += a;
+    sleep(1);
+}
+
+/* test size reporting */
+TEST(TaskQueue, Size) {
+    auto tq = TaskQueue<std::string>(handleQueue);
+    tq.pushToQueue("8");
+    tq.pushToQueue("9");
+
+    EXPECT_EQ(tq.size(), 2);
+    // give it some time
+    sleep(1);
+
+    EXPECT_EQ(result, std::string("89"));
+    // clear for next test
+    result = "";
+}
+
+/* Same as above but check size using push method */
+TEST(TaskQueue, SizeFromPush) {
+    auto tq = TaskQueue<std::string>(handleQueue);
+    tq.pushToQueue("8");
+    
+    EXPECT_EQ(tq.pushToQueue("9"), 2);
+    sleep(1);
+}
+
+/* Added some delay for task processing */
+TEST(TaskQueue, TimeConsumingTask) {
+    auto tq = TaskQueue<std::string>(handleQueueDelayed);
+    tq.pushToQueue("8");
+    
+    sleep(2);
+    EXPECT_EQ(tq.pushToQueue("9"), 1);
+    sleep(2);
+    EXPECT_EQ(tq.size(), 0);
+
+    EXPECT_EQ(result_delayed, std::string("89"));
+}
+
+/* Test queue stop 
+* ! job remains in fifo just queue is stopped
+*/
+TEST(TaskQueue, StopQueue) {
+    auto tq = TaskQueue<std::string>(handleQueueDelayed);
+    tq.pushToQueue("8");
+    tq.stopQueue();
+    
+    EXPECT_EQ(tq.size(), 1);
+}
