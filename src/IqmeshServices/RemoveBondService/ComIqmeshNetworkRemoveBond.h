2e93a2d0c0c3b2e34701a7d878bb9c05d82b74de
Karel Hanák <karel.hanak@iqrf.org>
2024-01-26 11:56:49 +0100

    RemoveBondService: updated implementation to allow unicast, multicast and broadcast removal of nodes, unified api
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/IqmeshServices/RemoveBondService/ComIqmeshNetworkRemoveBond.h
+++ src/IqmeshServices/RemoveBondService/ComIqmeshNetworkRemoveBond.h
@@ -17,92 +17,104 @@
 #pragma once
 
 #include "ComBase.h"
-#include <vector>
+#include <set>
 
 namespace iqrf {
 
-  // RemoveBond input paramaters
-  typedef struct
-  {
-    uint8_t deviceAddr = 0;
-    uint16_t hwpId = HWPID_DoNotCheck;
-    bool wholeNetwork = false;
-    int repeat = 1;
-    std::vector<uint8_t> deviceAddrList;
-    bool clearAllBonds = false;
-  } TRemoveBondInputParams;
+	/// RemoveBond input paramaters
+	typedef struct {
+		/// List of device addresses to remove
+		std::set<uint8_t> deviceAddrList;
+		/// Unbond all devices from network
+		bool allNodes = false;
+		/// Hardware profile ID filter
+		uint16_t hwpId = HWPID_DoNotCheck;
+		/// Coordinator only
+		bool coordinatorOnly = false;
+		/// Number of transaction repeats on failure
+		int repeat = 1;
+	} TRemoveBondRequestParams;
 
-  class ComIqmeshNetworkRemoveBond : public ComBase
-  {
-  public:
-    ComIqmeshNetworkRemoveBond() = delete;
-    explicit ComIqmeshNetworkRemoveBond(rapidjson::Document& doc) : ComBase(doc)
-    {
-      parse(doc);
-    }
+	class ComIqmeshNetworkRemoveBond : public ComBase {
+	public:
+		/// Delete default constructor
+		ComIqmeshNetworkRemoveBond() = delete;
 
-    virtual ~ComIqmeshNetworkRemoveBond()
-    {
-    }
+		/**
+		 * Constructor
+		 * @param doc Request document
+		 */
+		explicit ComIqmeshNetworkRemoveBond(rapidjson::Document& doc) : ComBase(doc) {
+			parse(doc);
+		}
 
-    const TRemoveBondInputParams getRomveBondParams() const
-    {
-      return m_removeBondInputParams;
-    }
+		/**
+		 * Destructor
+		 */
+		virtual ~ComIqmeshNetworkRemoveBond() {}
 
-  protected:
-    void createResponsePayload(rapidjson::Document& doc, const IDpaTransactionResult2& res) override
-    {
-      rapidjson::Pointer("/data/rsp/response").Set(doc, HexStringConversion::encodeBinary(res.getResponse().DpaPacket().Buffer, res.getResponse().GetLength()));
-    }
+		/**
+		 * Get request parameters
+		 * @return const TRemoveBondRequestParams
+		 */
+		const TRemoveBondRequestParams getRequestParameters() const {
+			return m_requestParams;
+		}
 
-  private:
-    TRemoveBondInputParams m_removeBondInputParams;
+	protected:
+		/**
+		 * Populate response document with response payload
+		 * @param doc Response document
+		 * @param res Transaction result
+		 */
+		void createResponsePayload(rapidjson::Document& doc, const IDpaTransactionResult2& res) override {
+			rapidjson::Pointer("/data/rsp/response").Set(doc, HexStringConversion::encodeBinary(res.getResponse().DpaPacket().Buffer, res.getResponse().GetLength()));
+		}
 
-    // parses document into data fields
-    void parse(rapidjson::Document& doc)
-    {
-      rapidjson::Value* jsonValue;
+	private:
+		/**
+		 * Parses request document into class members
+		 * @param doc Request document
+		 */
+		void parse(rapidjson::Document& doc) {
+			rapidjson::Value* jsonValue;
+			m_requestParams.deviceAddrList.clear();
+			// addresses specified
+			if ((jsonValue = rapidjson::Pointer("/data/req/deviceAddr").Get(doc))) {
+				/// integer
+				if (jsonValue->IsInt()) {
+					uint8_t addr = static_cast<uint8_t>(jsonValue->GetUint());
+					m_requestParams.deviceAddrList.insert(addr);
+				}
+				/// array of integers
+				if (jsonValue->IsArray()) {
+					for (auto itr = jsonValue->Begin(); itr != jsonValue->End(); ++itr) {
+						if (itr->IsInt()) {
+							uint8_t addr = static_cast<uint8_t>(itr->GetUint());
+							m_requestParams.deviceAddrList.insert(addr);
+						}
+					}
+				}
+			}
+			// entire network
+			if ((jsonValue = rapidjson::Pointer("/data/req/allNodes").Get(doc))) {
+				m_requestParams.allNodes = jsonValue->GetBool();
+			}
+			// hwpId
+			if ((jsonValue = rapidjson::Pointer("/data/req/hwpId").Get(doc))) {
+				m_requestParams.hwpId = static_cast<uint16_t>(jsonValue->GetUint());
+			}
+			// coordinator only
+			if ((jsonValue = rapidjson::Pointer("/data/req/coordinatorOnly").Get(doc))) {
+				m_requestParams.coordinatorOnly = jsonValue->GetBool();
+			}
+			// repeat
+			if ((jsonValue = rapidjson::Pointer("/data/repeat").Get(doc))) {
+				m_requestParams.repeat = jsonValue->GetInt();
+			}
+		}
 
-      // deviceAddress
-      if ((jsonValue = rapidjson::Pointer("/data/req/deviceAddr").Get(doc)))
-      {
-        m_removeBondInputParams.deviceAddrList.clear();
-
-        if (jsonValue->IsInt())
-        {
-          uint32_t addr = jsonValue->GetInt();
-          m_removeBondInputParams.deviceAddr = (uint8_t)addr;
-        }
-
-        if (jsonValue->IsArray())
-        {
-          for (auto itr = jsonValue->Begin(); itr != jsonValue->End(); ++itr)
-          {
-            if (itr->IsInt())
-            {
-              uint8_t addr = (uint8_t)itr->GetInt();
-              m_removeBondInputParams.deviceAddrList.push_back(addr);
-            }
-          }
-        }
-      }
-
-      // hwpId
-      if ((jsonValue = rapidjson::Pointer("/data/req/hwpId").Get(doc)))
-        m_removeBondInputParams.hwpId = (uint16_t)jsonValue->GetInt();
-
-      // wholeNetwork
-      if ((jsonValue = rapidjson::Pointer("/data/req/wholeNetwork").Get(doc)))
-        m_removeBondInputParams.wholeNetwork = jsonValue->GetBool();
-
-      // clearAllBonds
-      if ((jsonValue = rapidjson::Pointer("/data/req/clearAllBonds").Get(doc)))
-        m_removeBondInputParams.clearAllBonds = jsonValue->GetBool();
-
-      // repeat
-      if (rapidjson::Value* repeatJsonVal = rapidjson::Pointer("/data/repeat").Get(doc))
-        m_removeBondInputParams.repeat = repeatJsonVal->GetInt();
-    }
-  };
+		/// Request parameters
+		TRemoveBondRequestParams m_requestParams;
+	};
 }
