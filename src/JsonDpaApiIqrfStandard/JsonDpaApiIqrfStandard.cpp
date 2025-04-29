78cd30d9136d26a80acdb0e5af87a98901e2407f
Karel Hanák <karel.hanak@iqrf.org>
2025-04-29 17:31:22 +0200

    JsonDpaApiIqrfStandard: use status code and message to indicate that no response from network is required or expected

--- src/JsonDpaApiIqrfStandard/JsonDpaApiIqrfStandard.cpp
+++ src/JsonDpaApiIqrfStandard/JsonDpaApiIqrfStandard.cpp
@@ -395,10 +395,7 @@ namespace iqrf {
           }
           else {
             //no response but not considered as an error
-            Document rspObj;
-            Pointer("/response").Set(rspObj, "unrequired");
-            com->setPayload("/data/rsp/result", rspObj, false);
-            com->setStatus(res->getErrorString(), res->getErrorCode());
+            com->setStatus("No network response expected.", 256);
             com->createResponse(allResponseDoc, *res);
           }
         }
