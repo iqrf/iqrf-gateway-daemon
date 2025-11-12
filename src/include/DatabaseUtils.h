1481faf3e40081dc53350cb6a03389b4314a7f95
Karel Hanák <karel.hanak@iqrf.org>
2025-11-12 11:17:20 +0100

    WIP: testcases for websocket messaging authentication
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/include/DatabaseUtils.h
+++ src/include/DatabaseUtils.h
@@ -1,5 +1,23 @@
-#include "db_utils.h"
+#pragma once
 
+#include <memory>
+
+#include <SQLiteCpp/SQLiteCpp.h>
+
+/**
+ * @file db_utils.h
+ * @brief SQLiteCpp helpers for creating database connection.
+ */
+
+/**
+ * @brief Create a database connection handle.
+ *
+ * The function checks for existence of database file, attempts to create a connection,
+ * and checks if `api_tokens` table exists.
+ *
+ * @param path Path to SQLite database file
+ * @return Database connection handle
+ */
 std::shared_ptr<SQLite::Database> create_database_connetion(const std::string& path) {
   if (!std::filesystem::exists(path)) {
     throw std::invalid_argument("Database file does not exist.");
