28c132e081ffc47cc08e2afd31904c2e888bcf31
Karel Hanák <karel.hanak@iqrf.org>
2025-11-21 13:15:30 +0100

    IqrfDb: reset database and preserve tokens
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/DbEntities/repositories/api_token_repo.hpp
+++ src/DbEntities/repositories/api_token_repo.hpp
@@ -33,7 +33,8 @@ public:
     SQLite::Statement stmt(*m_db,
       R"(
       SELECT id, owner, salt, hash, createdAt, expiresAt, revoked, service
-      FROM api_tokens;
+      FROM api_tokens
+      ORDER BY id;
       )"
     );
     std::vector<ApiToken> vec;
@@ -57,6 +58,34 @@ public:
     stmt.bind(5, apiToken.getExpiresAt());
     stmt.bind(6, apiToken.isRevoked());
     stmt.bind(7, apiToken.canUseServiceMode());
+    try {
+    stmt.exec();
+    } catch (const SQLite::Exception &e) {
+      throw std::runtime_error(
+        this->formatErrorMessage(
+          "Failed to insert new ApiToken entity",
+          e.what()
+        )
+      );
+    }
+    return m_db->getLastInsertRowid();
+  }
+
+  uint32_t insertWithId(ApiToken& apiToken) {
+    SQLite::Statement stmt(*m_db,
+      R"(
+      INSERT INTO api_tokens (id, owner, salt, hash, createdAt, expiresAt, revoked, service)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+      )"
+    );
+    stmt.bind(1, apiToken.getId());
+    stmt.bind(2, apiToken.getOwner());
+    stmt.bind(3, apiToken.getSalt());
+    stmt.bind(4, apiToken.getHash());
+    stmt.bind(5, apiToken.getCreatedAt());
+    stmt.bind(6, apiToken.getExpiresAt());
+    stmt.bind(7, apiToken.isRevoked());
+    stmt.bind(8, apiToken.canUseServiceMode());
     try {
     stmt.exec();
     } catch (const SQLite::Exception &e) {
