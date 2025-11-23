125f6cc49dff0736510e07d847dce47f9b386b6a
Karel Hanák <karel.hanak@iqrf.org>
2025-11-23 11:27:55 +0100

    added API token service, separated api tokens into an auth database
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

---
+++ src/include/IApiTokenService.h
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2015-2025 IQRF Tech s.r.o.
+ * Copyright 2019-2025 MICRORISC s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "repositories/api_token_repo.hpp"
+
+namespace iqrf {
+
+  /**
+   * API token service
+   */
+  class IApiTokenService {
+  public:
+
+    /**
+     * Return API token entity by ID
+     * @param id API token ID
+     * @return API token entity
+     */
+    virtual std::unique_ptr<ApiToken> getApiToken(const uint32_t id) = 0;
+  };
+
+}
