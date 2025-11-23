c2c29ef9d5653ad3acab1f99108afb9b4402a0ea
Karel Hanák <karel.hanak@iqrf.org>
2025-11-23 15:37:34 +0100

    added migration manager tests
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/MigrationManager/MigrationManager.cpp
+++ src/MigrationManager/MigrationManager.cpp
@@ -22,101 +22,86 @@
 #include <algorithm>
 #include <filesystem>
 #include <fstream>
-#include <vector>
 
 namespace iqrf {
 
-  class MigrationManager::Impl {
-  private:
-    /**
-     * @brief Path to directory containnig migrations
-     */
-    const std::string directory;
-  public:
-    Impl(const std::string& path): directory(path) {
-      if (!std::filesystem::exists(directory)) {
-        throw std::invalid_argument("Migration directory " + directory + " does not exist.");
-      }
+  MigrationManager::MigrationManager(const std::string& path): directory(path) {
+    if (!std::filesystem::exists(directory)) {
+      throw std::invalid_argument("Migration directory " + directory + " does not exist.");
     }
-
-    std::size_t migrate(std::shared_ptr<SQLite::Database> connection) {
-      auto available = getAvailableMigrations();
-      std::size_t executed = 0;
-      std::vector<std::string> migrationsToExecute;
-		  std::set<std::string> executedMigrations;
-      if (connection->tableExists("migrations")) {
-        db::repos::MigrationRepository migrationRepo(connection);
-        executedMigrations = migrationRepo.getExecutedVersions();
-      }
-
-      for (auto &migration : available) {
-        if (executedMigrations.count(migration) == 0) {
-          migrationsToExecute.push_back(migration);
-        }
-      }
-
-      try {
-        for (const auto &migration : migrationsToExecute) {
-          executeMigation(connection, directory + migration + ".sql");
-          executed++;
-        }
-      } catch (const std::exception &e) {
-        throw std::runtime_error(e.what());
-      }
-      return executed;
-    }
-  private:
-    std::vector<std::string> getAvailableMigrations() {
-      std::vector<std::string> migrations;
-      for (const auto &file : std::filesystem::directory_iterator(directory)) {
-        if (file.is_regular_file()) {
-          migrations.push_back(file.path().stem());
-        }
-      }
-      std::sort(migrations.begin(), migrations.end());
-      return migrations;
-    }
-
-    void executeMigation(std::shared_ptr<SQLite::Database> connection, const std::string& migration) {
-      std::vector<std::string> statements;
-      // try to access migration file
-      std::ifstream migrationFile(migration);
-      if (!migrationFile.is_open()) {
-        throw std::runtime_error("Unable to read migration file: " + migration);
-      }
-      std::string line;
-      std::stringstream statementStream;
-      while (std::getline(migrationFile, line)) {
-        // remove comments and empty lines
-        if (line.empty() || line.rfind("--", 0) == 0) {
-          continue;
-        }
-        statementStream << line;
-      }
-      // split into separate statements
-      while (std::getline(statementStream, line, ';')) {
-        statements.push_back(line);
-      }
-      // check for empty file
-      if (statements.size() == 0) {
-        throw std::runtime_error("Empty migration file: " + migration);
-      }
-      try {
-			// execute migration statements
-        for (auto &statement : statements) {
-          connection->exec(statement);
-        }
-      } catch (const std::exception &e) {
-        throw std::runtime_error(e.what());
-      }
-    }
-  };
-
-  MigrationManager::MigrationManager(const std::string& path): impl_(std::make_unique<Impl>(path)) {}
-
-  MigrationManager::~MigrationManager() = default;
+  }
 
   std::size_t MigrationManager::migrate(std::shared_ptr<SQLite::Database> connection) {
-    return impl_->migrate(connection);
+    auto available = getAvailableMigrations();
+    std::size_t executed = 0;
+    std::vector<std::string> migrationsToExecute;
+    std::set<std::string> executedMigrations;
+    if (connection->tableExists("migrations")) {
+      db::repos::MigrationRepository migrationRepo(connection);
+      executedMigrations = migrationRepo.getExecutedVersions();
+    }
+
+    for (auto &migration : available) {
+      if (executedMigrations.count(migration) == 0) {
+        migrationsToExecute.push_back(migration);
+      }
+    }
+
+    try {
+      for (const auto &migration : migrationsToExecute) {
+        executeMigation(connection, directory + migration + ".sql");
+        executed++;
+      }
+    } catch (const std::exception &e) {
+      throw std::runtime_error(e.what());
+    }
+    return executed;
+  }
+
+  ///// Private methods /////
+
+  std::vector<std::string> MigrationManager::getAvailableMigrations() {
+    std::vector<std::string> migrations;
+    for (const auto &file : std::filesystem::directory_iterator(directory)) {
+      if (file.is_regular_file()) {
+        migrations.push_back(file.path().stem());
+      }
+    }
+    std::sort(migrations.begin(), migrations.end());
+    return migrations;
+  }
+
+  void MigrationManager::executeMigation(std::shared_ptr<SQLite::Database> connection, const std::string& migration) {
+    std::vector<std::string> statements;
+    // try to access migration file
+    std::ifstream migrationFile(migration);
+    if (!migrationFile.is_open()) {
+      throw std::runtime_error("Unable to read migration file: " + migration);
+    }
+    std::string line;
+    std::stringstream statementStream;
+    while (std::getline(migrationFile, line)) {
+      // remove comments and empty lines
+      if (line.empty() || line.rfind("--", 0) == 0) {
+        continue;
+      }
+      statementStream << line;
+    }
+    // split into separate statements
+    while (std::getline(statementStream, line, ';')) {
+      statements.push_back(line);
+    }
+    // check for empty file
+    if (statements.size() == 0) {
+      throw std::runtime_error("Empty migration file: " + migration);
+    }
+    try {
+    // execute migration statements
+      for (auto &statement : statements) {
+        connection->exec(statement);
+      }
+    } catch (const std::exception &e) {
+      throw std::runtime_error(e.what());
+    }
   }
 }
