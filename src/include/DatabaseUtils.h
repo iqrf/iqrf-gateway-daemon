1f500d52e344b10004578fb0cf2ce79433f56c3f
Karel Hanák <karel.hanak@iqrf.org>
2025-11-26 13:03:51 +0100

    ApiTokenService: use write-ahead logging journal mode
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/include/DatabaseUtils.h
+++ src/include/DatabaseUtils.h
@@ -16,15 +16,24 @@
  * and checks if `api_tokens` table exists.
  *
  * @param path Path to SQLite database file
+ * @param create Create database file if it does not exist
+ * @param busyTimeout Busy timeout
+ * @param wal Write-ahead logging journal mode
  * @return Database connection handle
  */
-std::shared_ptr<SQLite::Database> create_database_connetion(const std::string& path) {
-  if (!std::filesystem::exists(path)) {
+std::shared_ptr<SQLite::Database> create_database_connetion(const std::string& path, bool create = true, int busyTimeout = 3000, bool wal = true) {
+  if (!create && !std::filesystem::exists(path)) {
     throw std::invalid_argument("Database file does not exist.");
   }
   try {
-    auto db = std::make_shared<SQLite::Database>(path, SQLite::OPEN_READWRITE);
-    db->setBusyTimeout(3000);
+    int openFlags = SQLite::OPEN_READWRITE;
+    if (create) {
+      openFlags |= SQLite::OPEN_CREATE;
+    }
+    auto db = std::make_shared<SQLite::Database>(path, openFlags, busyTimeout);
+    if (wal) {
+      db->exec("PRAGMA journal_mode=WAL;");
+    }
     return db;
   } catch (const SQLite::Exception &e) {
     throw std::runtime_error("SQLite error: " + std::string(e.what()));
