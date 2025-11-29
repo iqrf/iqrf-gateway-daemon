135d4e1fb1ae039074cb461ace8d0551dbef39b4
Karel Hanák <karel.hanak@iqrf.org>
2025-11-29 11:48:43 +0100

    DatabaseUtilsTest: updated testcases for WAL mode changes
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/DatabaseUtilsTest.cpp
+++ tests/DatabaseUtilsTest.cpp
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 #include <DatabaseUtils.h>
+#include "sqlite3.h"
 
 class DatabaseUtilsTest : public ::testing::Test {
 protected:
@@ -30,21 +31,29 @@ TEST_F(DatabaseUtilsTest, create_database_connection_with_defaults) {
 
   SQLite::Statement journalPragma(*db, "PRAGMA journal_mode;");
   EXPECT_TRUE(journalPragma.executeStep());
-  EXPECT_STREQ("wal", journalPragma.getColumn(0).getText());
-
-  SQLite::Statement syncPragma(*db, "PRAGMA synchronous;");
-  EXPECT_TRUE(syncPragma.executeStep());
-  EXPECT_EQ(2, syncPragma.getColumn(0).getInt());
-}
-
-TEST_F(DatabaseUtilsTest, create_database_connetion_no_wal) {
-  auto db = create_database_connetion(dbPath, true, 3000, false);
-
-  SQLite::Statement journalPragma(*db, "PRAGMA journal_mode;");
-  EXPECT_TRUE(journalPragma.executeStep());
   EXPECT_STREQ("delete", journalPragma.getColumn(0).getText());
 
   SQLite::Statement syncPragma(*db, "PRAGMA synchronous;");
   EXPECT_TRUE(syncPragma.executeStep());
   EXPECT_EQ(2, syncPragma.getColumn(0).getInt());
+
+  SQLite::Statement busyTimeoutPragma(*db, "PRAGMA busy_timeout;");
+  EXPECT_TRUE(busyTimeoutPragma.executeStep());
+  EXPECT_EQ(3000, busyTimeoutPragma.getColumn(0).getInt());
+}
+
+TEST_F(DatabaseUtilsTest, create_database_connetion_wal_mode) {
+  auto db = create_database_connetion(dbPath, true, 500, true);
+
+  SQLite::Statement journalPragma(*db, "PRAGMA journal_mode;");
+  EXPECT_TRUE(journalPragma.executeStep());
+  EXPECT_STREQ("wal", journalPragma.getColumn(0).getText());
+
+  SQLite::Statement syncPragma(*db, "PRAGMA synchronous;");
+  EXPECT_TRUE(syncPragma.executeStep());
+  EXPECT_EQ(2, syncPragma.getColumn(0).getInt());
+
+  SQLite::Statement busyTimeoutPragma(*db, "PRAGMA busy_timeout;");
+  EXPECT_TRUE(busyTimeoutPragma.executeStep());
+  EXPECT_EQ(500, busyTimeoutPragma.getColumn(0).getInt());
 }
