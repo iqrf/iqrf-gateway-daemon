2cb41177f53b35e1360aabe0477baf0c40a6c7ab
Karel Hanák <karel.hanak@iqrf.org>
2026-03-02 20:30:04 +0100

    tokenctl utility: include token ID in output of newly created token as JSON
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- tests/ApiTokenCtl/cli_utils_test.cpp
+++ tests/ApiTokenCtl/cli_utils_test.cpp
@@ -165,7 +165,7 @@ TEST(cli_utils, construct_shareable_token_plain) {
 
 TEST(cli_utils, construct_shareable_token_json) {
   EXPECT_EQ(
-    "{\"token\":\"iqrfgd2;1;updgKmdhceGzmI9i0M9UELExYZwSKrCBv1vusq+TfHk=\"}",
+    "{\"id\":1,\"token\":\"iqrfgd2;1;updgKmdhceGzmI9i0M9UELExYZwSKrCBv1vusq+TfHk=\"}",
     construct_shareable_token(1, "updgKmdhceGzmI9i0M9UELExYZwSKrCBv1vusq+TfHk=", true)
   );
 }
