0d7d1423446d8960b56ce33c6a940543311196fd
Karel Hanák <karel.hanak@iqrf.org>
2026-03-26 17:11:49 +0100

    Websocket server: added configurable limit for clients
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

--- src/WebsocketServer/IWebSocketClientSession.h
+++ src/WebsocketServer/IWebSocketClientSession.h
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2015-2026 IQRF Tech s.r.o.
+ * Copyright 2019-2026 MICRORISC s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include <atomic>
@@ -14,7 +31,6 @@
 #include <string>
 #include <unordered_map>
 #include <utility>
-#include <vector>
 #include "WebsocketCallbackTypes.h"
 
 namespace iqrf {
@@ -199,20 +215,11 @@ namespace iqrf {
      */
     template<class SessionHandler>
     void forEachSession(SessionHandler&& handler) {
-      std::vector<std::shared_ptr<IWebSocketClientSession>> sessions;
-
-      {
-        std::lock_guard<std::mutex> lock(mutex_);
-        sessions.reserve(sessionStorage_.size());
-        for (auto [_, session] : sessionStorage_) {
-          if (auto sessionPointer = session.lock()) {
-            sessions.push_back(std::move(sessionPointer));
-          }
+      std::lock_guard<std::mutex> lock(mutex_);
+      for (auto [_, session] : sessionStorage_) {
+        if (auto sessionPointer = session.lock()) {
+          handler(sessionPointer);
         }
-      }
-
-      for (auto& session : sessions) {
-        handler(session);
       }
     }
 
